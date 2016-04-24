13c30da021b30b90a7408234c3afa1bcaa1f9d2b
Scott Heffner <scott.thomas.heffner@gmail.com>
2016-04-24 14:44:56 -0400

    Added mapLocation object used Google Maps API to create markers for those locations

--- js/main.js
+++ js/main.js
@@ -1,7 +1,34 @@
-var map;
+var mapLocation = function(lat, lng, title) {
+  this.latitude = lat;
+  this.longitude = lng;
+  this.title = ko.observable(title);
+};
+
+var ViewModel = function() {
+  locations = ko.observableArray([
+              new mapLocation(42.5750, -71.9826, 'The Big Chair'),
+              new mapLocation(42.5740603, -71.9958973, 'City Hall'),
+              new mapLocation(42.5741523, -71.9944446, 'Blue Moon Diner')
+              ]);
+};
+
+
+
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+  var myLatLng = {lat: 42.5750, lng: -71.9826};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: {lat: 42.5751, lng: -71.9981}
+  });
+
+  locations().forEach(function(location) {
+    new google.maps.Marker({
+      position: {lat: location.latitude, lng: location.longitude},
+      map: map,
+      title: location.title()
+    });
   });
 }
+
+ko.applyBindings(ViewModel);
