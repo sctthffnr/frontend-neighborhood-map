a0f1be9c5aee5073216a5af3ff8d6b392162f7ea
Scott Heffner <scott.thomas.heffner@gmail.com>
2016-04-24 20:08:22 -0400

    Separated concerns

--- js/main.js
+++ js/main.js
@@ -2,31 +2,40 @@ var mapLocation = function(lat, lng, title) {
   this.latitude = lat;
   this.longitude = lng;
   this.title = ko.observable(title);
+  this.visible = true;
 };
 
-var ViewModel = function() {
-  locations = ko.observableArray([
-              new mapLocation(42.5750, -71.9826, 'The Big Chair'),
-              new mapLocation(42.5740603, -71.9958973, 'City Hall'),
-              new mapLocation(42.5741523, -71.9944446, 'Blue Moon Diner')
-              ]);
+var ViewModel = {
+  locations: [
+    new mapLocation(42.5750, -71.9826, 'The Big Chair'),
+    new mapLocation(42.5740603, -71.9958973, 'City Hall'),
+    new mapLocation(42.5741523, -71.9944446, 'Blue Moon Diner')
+  ]
 };
 
+var View = {
+  renderMarkers: function() {
+    ViewModel.locations.forEach(function(location) {
+      location.marker.setMap(ViewModel.map);
+    });
+  }
+};
 
 function initMap() {
-
-  var map = new google.maps.Map(document.getElementById('map'), {
+  ViewModel.map = new google.maps.Map(document.getElementById('map'), {
     zoom: 13,
     center: {lat: 42.5751, lng: -71.9981}
   });
 
-  locations().forEach(function(location) {
-    new google.maps.Marker({
+  ViewModel.locations.forEach(function(location) {
+    location.marker = new google.maps.Marker({
       position: {lat: location.latitude, lng: location.longitude},
-      map: map,
       title: location.title()
     });
   });
+
+  View.renderMarkers();
 }
+
 
 ko.applyBindings(ViewModel);
