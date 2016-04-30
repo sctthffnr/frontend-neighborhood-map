8b7839842e4ba5c5c38981683d1959626e4e7d95
Scott Heffner <scott.thomas.heffner@gmail.com>
2016-04-30 09:54:40 -0400

    Ajax calls are only made the first time an InfoWindow is opened

--- js/main.js
+++ js/main.js
@@ -1,65 +1,25 @@
 function init() {
   "use strict";
 
-  // This is the constructor for a map location. The constructor includes
-  // ajax calls to populate the infoWindow of the object's Google maps marker.
+  // Constructor for mapLocation object
   var mapLocation = function(lat, lng, title) {
-    var self = this;
 
-    self.city = 'Gardner';
-    self.state = 'Massachusetts';
-    self.latitude = lat;
-    self.longitude = lng;
-    self.title = ko.observable(title);
-    self.show = ko.observable(true);
-    self.infoWindowOpen = false;
-    self.marker = new google.maps.Marker({
+    this.city = 'Gardner';
+    this.state = 'Massachusetts';
+    this.latitude = lat;
+    this.longitude = lng;
+    this.title = ko.observable(title);
+    this.show = ko.observable(true);
+    this.infoWindowOpen = false;
+    this.marker = new google.maps.Marker({
       position: {lat: this.latitude, lng: this.longitude},
       title: this.title(),
       animation: google.maps.Animation.DROP
     });
-    self.infoWindow = new google.maps.InfoWindow({
+    this.infoWindow = new google.maps.InfoWindow({
       position: {lat: 42.5751, lng: -71.9981},
       content: ''
     });
-
-    self.wikipediaCallback = function(data) {
-      var id = data.query.pages;
-      var text = id[Object.keys(id)[0]].extract;
-      if (!text) {
-        text = 'No Wikipedia entry for ' + self.title();
-      }
-      self.formatWikipedia(text);
-    };
-
-    self.formatWikipedia = function(text) {
-      var header = '<h2>Wikipedia Entry</h2>';
-      text = '<p>' + text + '</p>';
-      var html = header + text;
-      self.updateInfoWindow(html);
-    };
-
-    self.getFlickr = function() {
-      var url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search';
-      var api_key = '816dfffa932b12dc03313acf79610d73';
-      var query = self.title() + ' ' + self.city + ' ' + self.state;
-       $.ajax({
-        url: url + '&text=' + query + '&api_key=' + api_key + '&per_page=10&format=json&nojsoncallback=1',
-        dataType: 'json',
-        success: function(data) {
-          self.flickrCallback(data);
-        }
-      });
-    };
-
-    self.flickrCallback = function(data) {
-      self.updateInfoWindow('<h2>Pictures from Flickr</h2>');
-      data.photos.photo.forEach(function(photo) {
-        var img = 'https://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '.jpg';
-        var html = '<img src="' + img + '">';
-        self.updateInfoWindow(html);
-      });
-    };
   };
 
   // Prototype definitions for mapLocation class
@@ -75,6 +35,62 @@ function init() {
     });
   };
 
+  mapLocation.prototype.wikipediaCallback = function(data) {
+    var id = data.query.pages;
+    var text = id[Object.keys(id)[0]].extract;
+    if (!text) {
+      text = 'No Wikipedia entry for ' + this.title();
+    }
+    this.formatWikipedia(text);
+  };
+
+  mapLocation.prototype.formatWikipedia = function(text) {
+    var header = '<h2>Wikipedia Entry</h2>';
+    text = '<p>' + text + '</p>';
+    var html = header + text;
+    this.updateInfoWindow(html);
+  };
+
+  mapLocation.prototype.getFlickr = function() {
+    var self = this;
+    var url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search';
+    var api_key = '816dfffa932b12dc03313acf79610d73';
+    var query = self.title() + ' ' + self.city + ' ' + self.state;
+     $.ajax({
+      url: url + '&text=' + query + '&api_key=' + api_key + '&per_page=10&format=json&nojsoncallback=1',
+      dataType: 'json',
+      success: function(data) {
+        self.flickrCallback(data);
+      }
+    });
+  };
+
+  mapLocation.prototype.flickrCallback = function(data) {
+    var self = this;
+    self.updateInfoWindow('<h2>Pictures from Flickr</h2>');
+    data.photos.photo.forEach(function(photo) {
+      var img = 'https://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '.jpg';
+      var html = '<img src="' + img + '">';
+      self.updateInfoWindow(html);
+    });
+  };
+
+  mapLocation.prototype.toggleInfoWindow = function() {
+    if (this.infoWindow.content === '') {
+      this.getInfo();
+    }
+    if (this.infoWindowOpen) {
+      this.closeInfoWindow();
+    } else {
+      this.openInfoWindow();
+    }
+  };
+
+  mapLocation.prototype.getInfo = function() {
+    this.getWikipedia();
+    this.getFlickr();
+  };
+
   mapLocation.prototype.closeInfoWindow = function() {
     this.infoWindow.close();
     this.marker.setAnimation(null);
@@ -87,19 +103,9 @@ function init() {
     this.infoWindowOpen = true;
   };
 
-  mapLocation.prototype.toggleInfoWindow = function() {
-    if (this.infoWindowOpen) {
-      this.closeInfoWindow();
-    } else {
-      this.openInfoWindow();
-    }
-  };
-
   mapLocation.prototype.updateInfoWindow = function(data) {
     var updated = this.infoWindow.content + data;
-    this.infoWindow = new google.maps.InfoWindow({
-      content: updated
-    });
+    this.infoWindow.setContent(updated);
   };
 
   var ViewModel = {
@@ -132,8 +138,6 @@ function init() {
           location.marker.setMap(null);
         }
         location.marker.addListener('click', location.toggleInfoWindow.bind(location));
-        location.getWikipedia();
-        location.getFlickr();
       });
     },
 
